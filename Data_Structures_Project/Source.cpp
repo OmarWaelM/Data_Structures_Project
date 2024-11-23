a299014dd1df6991ecb3a5091b1df403a30273ce
OmarWaelM <omwamo05@gmail.com>
2024-11-23 11:45:12 +0200

    output functions for car and patient

--- Data_Structures_Project/Source.cpp
+++ Data_Structures_Project/Source.cpp
@@ -9,5 +9,10 @@ int main()
 	cin >> x;
 	printf("\033c");
 	cout << "after clear" << endl;
+	Patient P1(1, 5, 2, 15, EP, 5);
+	Car C1(15, 2, SC, 15);
+	C1.AssignPatient(&P1);
+	cout << "Patient1: " << P1 << endl;
+	cout << "Car1: " << C1 << endl;
 	return 0;
 }
