8ab57e2cf5018e8f9de154847ff43abe5dcac7f8
arwaibrahiim <166602987+arwaibrahiim@users.noreply.github.com>
2024-11-23 21:02:34 +0200

    a

--- Data_Structures_Project/ModifiedQ.h
+++ Data_Structures_Project/ModifiedQ.h
@@ -1,9 +1,14 @@
 #pragma once
-
-
 template <typename T>
 class ModifiedQ : public LinkedQueue<T>
 {
-
+public:
+	ModifiedQ(const LinkedQueue<T>& LQ);
+	
 };
 
+template<typename T>
+ ModifiedQ<T>::ModifiedQ(const LinkedQueue<T>& LQ)
+{
+	 
+}
