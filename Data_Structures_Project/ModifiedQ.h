11f90863e3fe1cfd32a1d59c2dda36157b98a835
arwaibrahiim <166602987+arwaibrahiim@users.noreply.github.com>
2024-11-26 00:06:57 +0200

    ret type update

--- Data_Structures_Project/ModifiedQ.h
+++ Data_Structures_Project/ModifiedQ.h
@@ -1,15 +1,28 @@
 #pragma once
+class LinkedQueue;
 template <typename T>
 class ModifiedQ : public LinkedQueue<T>
 {
 public:
 
-	void cancelNPRequest(Patient* patient);
+	bool cancelNPRequest(Patient* patient);
 	
 };
 
 template<typename T>
- void ModifiedQ<T>::cancelNPRequest(Patient* patient)
+ bool ModifiedQ<T>::cancelNPRequest(Patient* patient)
 {
+	 if (isEmpty())
+		 return false;
+	 Node<T>* nodeToDeletePtr = frontPtr;
+	 frntEntry = frontPtr->getItem();
+	 frontPtr = frontPtr->getNext();
+	 // Queue is not empty; remove front
+	 if (nodeToDeletePtr == backPtr)	 // Special case: last node in the queue
+		 backPtr = nullptr;
 
-}
+	 // Free memory reserved for the dequeued node
+	 delete nodeToDeletePtr;
+	 count--;
+	 return true;
+ }
