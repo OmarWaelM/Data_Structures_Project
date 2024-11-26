3208f2a16130c8e7a87d25a5343ef1de19f80405
arwaibrahiim <166602987+arwaibrahiim@users.noreply.github.com>
2024-11-27 00:53:01 +0200

    j

--- Data_Structures_Project/Hospital.h
+++ Data_Structures_Project/Hospital.h
@@ -22,7 +22,6 @@ public:
 	void addCarToList(Car* car);
 	void addPatientToList(Patient* patient);
 	bool assignPatientToCar(Patient* patient);
-	bool cancelNPRequest(Patient* NP);
 	friend ostream& operator <<(ostream& os, Hospital& h);
 
 };
@@ -85,10 +84,7 @@ bool Hospital::assignPatientToCar(Patient* p)
 	return false;
 }
 
-bool Hospital::cancelNPRequest(Patient* NP)
-{
-	return(NPList.cancelRequest(NP));
-}
+
 
 
 //this can be changed i made it to look like the description
