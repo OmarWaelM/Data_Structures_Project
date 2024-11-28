b8c0a574a5f6a6bbdf6c454be342a7474862b45a
tokamohamed03 <toka090903@gmail.com>
2024-11-28 21:47:07 +0200

    k

--- Data_Structures_Project/Hospital.h
+++ Data_Structures_Project/Hospital.h
@@ -26,7 +26,7 @@ private:
 public:
 	//Member Function
 	Hospital();
-	Hospital(int ID) { hospitalID = ID; }
+	Hospital(int ID) { hospitalID = ID; scCount = 0; ncCount = 0; }
 	void setID(int id) { hospitalID = id; }
 	int getHospitalID() const { return hospitalID; }
 	void setSCarsCount(int count) { scCount = count; }
