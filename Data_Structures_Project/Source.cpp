3515142e063c95ea6d78ce2b10a00c9ec54837e0
tokamohamed03 <toka090903@gmail.com>
2024-11-28 19:22:58 +0200

    ll

--- Data_Structures_Project/Source.cpp
+++ Data_Structures_Project/Source.cpp
@@ -1,4 +1,4 @@
-/*#include <iostream>
+#include <iostream>
 #include <Windows.h>
 #include <string>
 #include <fstream>
@@ -18,7 +18,7 @@
 
 using namespace std;
 
-int main() 
+int main() {
 	int x;
 	cout << "before clear" << endl;
 	cin >> x;
@@ -43,4 +43,4 @@ int main()
 	Organizer Org;
 	Org.Simulator();
 	return 0;
-} */
+}
