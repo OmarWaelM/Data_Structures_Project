4b97f70f4b4bde29b1c4cede5fc0b0ce056c59d3
tokamohamed03 <toka090903@gmail.com>
2024-11-29 16:49:57 +0200

    ll

--- Data_Structures_Project/Organizer.h
+++ Data_Structures_Project/Organizer.h
@@ -14,18 +14,17 @@ class Organizer
 {
 private:
 	//Lists used in orgranizer class
-	LinkedQueue<Patient*> patientsList;  // Patients list queue (list of pointers to patients)
-	LinkedQueue<CancellationReq> CancellationList;
-	LinkedQueue<Patient*> FinishedList;
-	priQueue<Car*> BackCars;
-	ModifiedPriQ OutCars;
+	Hospital** HospitalList; //An array of pointers to hospitals
+	LinkedQueue<Patient*> patientsList;  // Patients list of type Linked Queue (list of pointers to patients)
+	LinkedQueue<CancellationReq> CancellationList; // Cancellation requests' list of type Linked Queue
+	LinkedQueue<Patient*> FinishedList; //Finished patients' list of type Linked Queue
+	priQueue<Car*> BackCars; // Back cars' list (cars on their way back) of type Priority Queue
+	ModifiedPriQ OutCars; // Out cars' list (cars out on their way to pick up patients) of type Priority Queue (modified)
 
 	//General data members
 	int timeStep;
 	UI GUI;
 	string filename;
-
-	Hospital** HospitalList; //An array of pointers to hospitals
 	int numHospitals;
 	int speedScars, speedNcars;
 	int** distanceMatrix;
